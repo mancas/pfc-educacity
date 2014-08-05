90cea71bb3a5a4b857c56c9aba20d99a3429e231
manu <manu@manu-VirtualBox.(none)>
2014-08-05 19:22:28 +0200

    drwaer

--- src/com/mancas/educacity/NavigationDrawerFragment.java
+++ src/com/mancas/educacity/NavigationDrawerFragment.java
@@ -1,18 +1,16 @@
 package com.mancas.educacity;
 
 
-import com.mancas.adapters.NavigationDrawerAdapter;
-
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,9 +18,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.mancas.adapters.NavigationDrawerAdapter;
 
 /**
  * Fragment used for managing interactions for and presentation of a navigation drawer.
